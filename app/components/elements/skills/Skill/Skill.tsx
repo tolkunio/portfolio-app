79e76da9cf8ee269005d3aff407a454281d3129e
Tolkun Omurbekova <tolkun3922@gmail.com>
2024-03-08 21:23:14 +0600

    -added skills section

--- app/components/elements/skills/Skill/Skill.tsx
+++ app/components/elements/skills/Skill/Skill.tsx
@@ -1,8 +1,14 @@
 import s from './Skill.module.scss';
-const Skill = () => {
+import Image from "next/image";
+type PropsType={
+    title:string,
+    iconUrl:string
+}
+const Skill = ({title,iconUrl}:PropsType) => {
     return (
         <div className={s.skill}>
-
+            <Image className={s.img} src={iconUrl} width={80} height={80} alt={'iconUrl'}/>
+            <span className={s.title}>{title}</span>
         </div>
     );
 };
--- app/components/elements/skills/Skills.tsx
+++ app/components/elements/skills/Skills.tsx
@@ -1,5 +1,50 @@
 import s from './Skills.module.scss'
 import SectionTitle from "@/app/components/common/sectionTitle/SectionTitle";
+import Skill from "@/app/components/elements/skills/Skill/Skill";
+
+const skills = [
+    {
+        title: 'Javascript',
+        iconUrl: '/icons/js.svg'
+    },
+    {
+        title: 'Typescript',
+        iconUrl: '/icons/ts.svg'
+    }, {
+        title: 'React',
+        iconUrl: '/icons/react.svg'
+    }, {
+        title: 'Redux',
+        iconUrl: '/icons/redux.svg'
+    }, {
+        title: 'NextJS',
+        iconUrl: '/icons/nextjs'
+    }, {
+        title: 'Html5',
+        iconUrl: '/icons/html.svg'
+    }, {
+        title: 'Css3',
+        iconUrl: '/icons/css.svg'
+    }, {
+        title: 'Styled Components',
+        iconUrl: '/icons/styled.svg'
+    }, {
+        title: 'NodeJS',
+        iconUrl: '/icons/node.svg'
+    }, {
+        title: 'RestApi',
+        iconUrl: '/icons/rest.svg'
+    },
+    {
+        title: 'Git',
+        iconUrl: '/icons/git.svg'
+    }, {
+        title: 'PostgreSql',
+        iconUrl: '/icons/postgresql.svg'
+    },
+
+
+]
 const Skills = () => {
     return (
         <section className={s.skills}>
@@ -7,7 +52,9 @@ const Skills = () => {
                 <SectionTitle>{`</Мои навыки/>`}</SectionTitle>
             </div>
             <div className={s.container}>
-
+                <div className={s.flexWrapper}>
+                    {skills.map(skill => <Skill key={skill.title} title={skill.title} iconUrl={skill.iconUrl}/>)}
+                </div>
             </div>
         </section>
     );
