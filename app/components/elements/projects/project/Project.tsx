b6fa80fb9b6dad7aa3b0c524cbfcf9bca12db019
Tolkun Omurbekova <tolkun3922@gmail.com>
2024-03-13 17:16:50 +0600

    -refactor project section

--- app/components/elements/projects/project/Project.tsx
+++ app/components/elements/projects/project/Project.tsx
@@ -11,7 +11,7 @@ type PropsType = {
 export const Project = ({imgUrl, title, desc, codeUrl, techStack}: PropsType) => {
     return (
         <div className={s.project}>
-            <Image src={imgUrl} width={413} height={257} alt='project img'/>
+                <Image src={imgUrl} width={360} height={257} alt='project img'/>
             <div className={s.content}>
                 <div className={s.descContent}>
                     <span className={s.title}>{title}</span>
