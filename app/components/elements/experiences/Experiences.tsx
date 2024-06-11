dcc87cc2299177861200ec85336ffbd2511bf1d9
Tolkun Omurbekova <tolkun3922@gmail.com>
2024-06-11 16:49:39 +0600

    -updated info

--- app/components/elements/experiences/Experiences.tsx
+++ app/components/elements/experiences/Experiences.tsx
@@ -6,11 +6,11 @@ import Image from "next/image";
 
 const experiences: IExperiences[] = [
     {
-        title: 'Frontend разработчик',
+        title: 'Junior Frontend разработчик',
         desc: 'Принимала участие в разработке SPA приложений с использованиемReact/TS/Redux/Next.js Оптимизировала производительность работы системы (memo,useCallback , useMemo и т.д). Занималась устранением ошибок и провела рефакторинг кода. Участвовала в коммуникации и взаимодействии с backend. Работала в команде через Git, Jira, активно проводила code review',
     },
     {
-        title: 'Junior Frontend разработчик',
+        title: 'Intern  Frontend разработчик',
         desc: 'Участвовала в анализе и подборе библиотек необходимых длявыполнения ТЗ.Реализовала управление состояниями на уровне BLL с помошью Redux Toolkit .Настраивала связь с сервером с использованием Axios (CRUD). Осуществляла авторизационный flow',
     },
     {
@@ -21,11 +21,11 @@ const experiences: IExperiences[] = [
 const companies: IExperiences[] = [
     {
         title: 'It Calendar Lab',
-        desc: '(2024 Feb - current)'
+        desc: '(2024 March - current)'
     },
     {
-        title: 'Cash Landing Co',
-        desc: '(2023 June - 2024 Feb)'
+        title: 'IT incubator',
+        desc: '(2024 January - 2024 March)'
     },
     {
         title: 'Instant Payments',
